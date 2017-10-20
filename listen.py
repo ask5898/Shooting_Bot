15f57410fdf0e739aa97bed4956fa3b6da1c3404
Ali Shahbaaz Khan <alishahbaazk@gmail.com>
2017-10-20 19:10:49 +0530

    Add files via upload

--- listen.py
+++ listen.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 
-import sys
 import pypot.dynamixel
 import time
 import itertools
@@ -8,105 +7,79 @@ import numpy as np
 import xml.etree.ElementTree as ET
 import cv2
 import numpy as np
-from threading import Thread
 from math import pi,atan,sin,cos,degrees
-#import rospy
-#from std_msgs.msg import String
+import rospy
+from std_msgs.msg import String
 
 #ang=(91.38, 87.34, 6.81, -47.16, 79.87, -80.31, -94.9, 124.18, -0.31, -2.68, 11.47, -12.7, -15.78, 14.55, -8.48, 3.91, -0.13, -4.26, 46.99)
 darwin = {1: 90, 2: -90, 3: 67.5, 4: -67.5, 7: 45, 8: -45, 9: 'i', 10: 'i', 13: 'i', 14: 'i', 17: 'i', 18: 'i'}
 abmath = {11: 15, 12: -15, 13: -10, 14: 10, 15: -5, 16: 5}
 hand = {5: 60, 6: -60}
-port = input(sys.argv[0])
+
 path = "/home/ali/catkin/src/walk/scripts/data.xml"
-
-
-def sniper1():
 	
-	s=0
-	
-	cap = cv2.VideoCapture(1)
-	y,u,v = 166,87,102
-	while True:
-		_,img = cap.read()
+y,u,v = 0,142,56
+
+cap = cv2.VideoCapture(2)
+
+def sniper():
+
+	rec=True	
+	while rec:
+		rec,img = cap.read()
 		img_yuv = cv2.cvtColor(img,cv2.COLOR_BGR2YUV)
-		copy = img.copy()
-		blur = cv2.GaussianBlur(img_yuv,(7,7),2)
-
-
-		die = cv2.inRange(blur, (np.array([y-45,u-30,v-30])), (np.array([y+45,u+30,v+30])))
-		#cv2.imshow("The Masked Image",die)
-		im_floodfill = die.copy()
-	 	h, w = die.shape[:2]
+	
+		blur = cv2.GaussianBlur(img_yuv,(11,11),2)
+		ball = cv2.inRange(blur, (np.array([0,u-30,v-30])), (np.array([255,u+30,v+30])))
+		im_floodfill = ball.copy()
+		h, w = ball.shape[:2]
 		mask = np.zeros((h+2, w+2), np.uint8)
-		abcd = cv2.floodFill(im_floodfill, mask, (0,0), 255);
-		    
-		fill = cv2.bitwise_and(im_floodfill,im_floodfill,mask = die)
-		
-		#cv2.imshow("Masked filled",fill)
-	 	if cv2.waitKey(25)&0xff==27:
-			break
+		cv2.floodFill(im_floodfill, mask, (0,0), 255)
+		fill = cv2.bitwise_and(im_floodfill,im_floodfill,mask = ball)
 
-		images,contours,hierarchy = cv2.findContours(fill,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+		if cv2.waitKey(25)&0xff==27:
+		    break
 
-		cv2.drawContours(img, contours, -1, (255,0,255), 2)
+		cv2.rectangle(img, (310,230), (330,250), (255,255,255),2)
+		crop_img = fill[230:250, 310:330]
+	
+		images,s_contour,hierarchy = cv2.findContours(crop_img,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+		images,contour,hierarchy = cv2.findContours(fill,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
+
+		cv2.drawContours(img, contour, -1, (0,255,0), 2)
+
 		cv2.imshow("",img)
-		print len(contours)
+		cv2.imshow("mask",im_floodfill)
+		
 
-		s = s+len(contours)
-
-		print s
-		if s >= 40 :
-			print "hi"
-			cv2.imwrite("ball.jpg", copy)
+		if len(s_contour)>=1:
 			return True
-			break
+	
+		elif len(contour)>=1:
+			cnt = contour[0]
+			(x,y),radius = cv2.minEnclosingCircle(cnt)
+			center = (int(x),int(y))
+			#point = cv2.circle(img,center,radius,(0,0,255),2)
+			if center[0]>320 and center[1]>240:
+				print "1,1"
+			if center[0]<320 and center[1]>240:
+				print "0,1"
+			if center[0]>320 and center[1]<240:
+				print "1,0"
+			if center[0]<320 and center[1]<240:
+				print "0,0"
+
+			
+
 		else :
 			return False
 
 	cv2.destroyAllWindows()
 	cap.release()
 
-def sniper2():
-	y,u,v = 166,87,102
-
-	img =cv2.imread("ball.jpg")
-	img_yuv = cv2.cvtColor(img,cv2.COLOR_BGR2YUV)
-
-	blur = cv2.GaussianBlur(img_yuv,(7,7),2)
 
 
-	die = cv2.inRange(blur, (np.array([y-45,u-30,v-30])), (np.array([y+45,u+30,v+30])))
-	#cv2.imshow("The Masked Image",die)
-	im_floodfill = die.copy()
-	h, w = die.shape[:2]
-	mask = np.zeros((h+2, w+2), np.uint8)
-	abcd = cv2.floodFill(im_floodfill, mask, (0,0), 255);
-	    
-	fill = cv2.bitwise_and(im_floodfill,im_floodfill,mask = die)
-
-	#cv2.imshow("Masked filled",fill)
-
-
-	images,contours,hierarchy = cv2.findContours(fill,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
-
-	#cv2.drawContours(img, contours, -1, (255,0,255), 2)
-	#cv2.imshow("",img)
-	print len(contours)
-
-
-	cnt = contours[0]
-
-	(x,y),radius = cv2.minEnclosingCircle(cnt)
-	center = (int(x),int(y))
-	radius = int(radius)
-	point = cv2.circle(img,center,radius,(0,0,255),2)
-
-	#cv2.imshow("abcd",point)
-	cv2.imwrite("img.jpg",point)
-
-	print center
-	print radius
 
 
 class Dynamixel(object) :
@@ -118,11 +91,12 @@ class Dynamixel(object) :
 		print "Is port se judna hai",ports[0]
 
 		self.dxl = pypot.dynamixel.DxlIO(ports[0])
-		self.ids = self.dxl.scan(range(20))
+		self.ids = self.dxl.scan(range(25))
 		print self.ids
 		self.dxl.enable_torque(self.ids)
-		if len(self.ids)<19 :
+		if len(self.ids)<20 :
 			raise RuntimeError("kuch motor bhakchodi pel rahe hain")
+		self.dxl.set_moving_speed(dict(zip(self.ids,itertools.repeat(100))))
 
 
 	def setSpeed(self,speed,ids) :
@@ -178,7 +152,7 @@ class Walk(object) :
 		self.begin = {}
 		self.end = {}
 		if not(p_pose) :
-			self.frame_diff = 10
+			self.frame_diff = 1
 			p_pose = pose
 		else :
 			self.frame_diff = self.frame-int(p_frame) 
@@ -213,107 +187,103 @@ class Walk(object) :
 		print self.end
 		write=[]
 		ids=[]
-		f_d=abs(self.frame_diff/10)
 		for key in self.end.keys() :
-			#pose_diff=abs(self.end[key]-self.begin[key])
-			linp=np.linspace(self.end[key],self.begin[key],f_d)
+			linp=np.linspace(self.end[key],self.begin[key],self.frame_diff)
 			write.append(linp)
-			#write.append(self.begin[key])
 			ids.append(key)	
+
 		print "out"
 		for pose in zip(*write) :
 			print "in"
 			x.setPos(dict(zip(ids,pose)))
-			time.sleep(0.1)
-
-	
-class Head(object) :
-	def __init__(self) :
-	
-		self.head=19
-		self.turn_right=0
-		self.turn_left=93
-		self.centre_pos=41
-		self.diff_left=abs(self.centre_pos-self.turn_left)
-		self.diff_right=abs(self.centre_pos-self.turn_right)
-		#x.setSpeed(10,fire_ids[-1:])
-		self.detect = False
-		#x.setSpeed(50,fire_ids[:-1])
-
-	def head_right(self) :
-		x.setSpeed(20,[self.head])
-		#linp=np.linspace(self.centre_pos,self.turn_right,self.diff_right)
-		
-		pose={self.head:self.turn_right}
-		print pose
-	
-		x.dictWrite(pose)
-
-	def head_left(self) :
-		x.setSpeed(20,[self.head])
-		#linp=np.linspace(self.centre_pos,self.turn_left,self.diff_left)
-		pose={self.head:self.turn_left}
-		print pose
-		x.dictWrite(pose)
-		
-	def to_centre(self) :
-		x.setSpeed(20,[self.head])
-		#linp=np.linspace(self.turn_left,self.centre_pos,self.diff_left)
-		pose={self.head:self.centre_pos}
-		print pose
-		x.dictWrite(pose)
-
-	
-h=Head()
+			time.sleep(0.001)
 
 class Motion(object) :
 	def __init__(self) : 
 		
 		
-		self.fire_ids=[2,4,6]
-		self.armangle=87.52
-		self.t=1
-		self.d=6
+		self.fire_left = [2,4,6]
+		self.fire_right = [1,3,5]
+		self.armangle = 87.52
+		self.d = 0.6
 		self.shoot()
 		
 
 	def shoot(self) :
-		head=list(x.returnPos(19))[0]
-		print head
-		alpha=float(atan(self.d+self.t*sin(head))/self.t*cos(head))
-		write=[self.armangle,alpha,-30.02]
-		print degrees(alpha)
-		pos=dict(zip(self.fire_ids,write))
-		x.dictWrite(pos)
-		print pos
+		head = list(x.returnPos(19))[0]
+		tilt = list(x.returnPos(20))[0]
+		if head > 0 :
+			write=[self.armangle,head-140,-30.02]
+			pos=dict(zip(self.fire_left,write))
+			x.dictWrite(pos)
+			time.sleep(3)
+			x.angleWrite(2,0)
+			time.sleep(0.01)
+			x.angleWrite(4,0.92)
+			print pos
 
+		elif head < 0 :
+			write = [self.armangle,head+140,-30.02]
+			pos = dict(zip(self.fire_right,write))
+			x.dictWrite(pos)
+			time.sleep(3)
+			x.angleWrite(2,0)
+			time.sleep(0.01)
+			x.angleWrite(4,0.92)
+			print pos
 
+						
 
-if __name__ == "__main__" :
+def walk() :
+	w1 = xml.parse("32 F_S_L")
+	time.sleep(0.01)
+	w2 = xml.parse("33 ")
+	time.sleep(0.01)
+	while True :
+		w3 = xml.parse("38 F_M_R")
+		time.sleep(0.01)	
+		w4 = xml.parse("39 ")
+		time.sleep(0.01)
+		w5 = xml.parse("36 F_M_L")
+		time.sleep(0.01)
+		w6 = xml.parse("37 ")
+		time.sleep(0.01)
 
+if __name__=="__main__" :
+	m_20 = 150
+	x.angleWrite(20,m_20)
 	balance = xml.parse("152 Balance")
 	raw_input("Proceed?")
-	sniper_final = Thread(target = sniper1 , args = ())
-	sniper_final.daemon = True
-	sniper_final.start()
+	#walk()
+	ghost_final = sniper()
+	detected = False
 	
 	while True :
 		
-		for pose in range(-90,90,10) :
-			fire = Thread(target = x.angleWrite , args = (19,pose,))
-			fire.daemon = True
-			fire.start()
+		for pose in range(-90,90,1) :
+			x.angleWrite(19,pose)
 			
 			time.sleep(0.1)
-			if sniper1() :
+			if sniper() :
+				detected = True
 				m = Motion()
-				fire.sleep()
+				time.sleep(0.01)
 
-		for pose in range(90,-90,-10) :
-			fire = Thread(target = x.angleWrite , args = (19,pose,))
-			fire.start()
+		
+		for pose in range(90,-90,-1) :
+			x.angleWrite(19,pose)
 			time.sleep(0.1)
-			if sniper1() :
+			if sniper() :
+				detected = True
 				m = Motion()
-				fire.sleep()
+				time.sleep(0.01)
+
+		if not detected :
+			m_20 = m_20-10
+			x.angleWrite(20,m_20)
+			
+
+		detected = False
+
+
 			
